1f1ffc72151c678d567352c9baf5eb084a3815e4
Robert Kidd <robert.kidd@gmail.com>
2023-04-27 19:10:45 -0500

    Chrome compatibility

--- scan.js
+++ scan.js
@@ -1,10 +1,13 @@
 (function () {
+    if(typeof browser === "undefined") {
+        var browser = chrome;
+    }
+
     let t = document.firstChild.textContent.slice(0, 9);
     if(t.toLowerCase() === "@database") {
         console.log("smells like amigaguide");
 
         const storage_name = "guidetext";
-        // localStorage.setItem(storage_name, document.firstChild.textContent);
         browser.runtime.sendMessage({
             method: "setGuideText",
             body: document.firstChild.textContent,
@@ -18,8 +21,6 @@
             p.set("storage", encodeURI(storage_name));
             const parent_ref = window.location.href.split("/").slice(0, -1).join("/");
             p.set("parent_ref", encodeURI(parent_ref));
-            // p.set("guide", encodeURI(window.location));
-            // p.set("aguide_text", encodeURI(document.firstChild.textContent));
             window.location = aguide_url + "?" + p.toString();
         });
     }
